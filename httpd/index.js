e7027f4fa211a9c6334a84433e8c6f284105278f
c5728 <a957c0lb@gmail.com>
2017-11-03 16:09:16 +0800

    1103-2

--- httpd/index.js
+++ httpd/index.js
@@ -3,46 +3,81 @@
 let http = require('http');
 
 http.createServer((request, response) => {
-let fs = require('fs');
-let postData = ''; // POST 資料
+  let fs = require('fs');
+  let postData = ''; // POST 資料
 
-// 利⽤'data' event 消耗掉data chunk;
-// 'end' event 才會被fired
-request.on('data', (chunk) => {
-postData += chunk;
+  // 利⽤'data' event 消耗掉data chunk;
+  // 'end' event 才會被fired
+  request.on('data', (chunk) => {
+    postData += chunk;
 
-console.log(
-' 接收的POST data ⽚段k: [' + chunk + '].'
-);
-});
+    console.log(
+      ' 接收的POST data ⽚段k: [' + chunk + '].'
+    );
+  });
 
-request.on('end', () => {
-switch (request.url) {
-case '/':
-fs.readFile('../htdocs/index.html', (err, data) => {
-if (err) {
-console.log(' 檔案讀取錯誤');
-}
-else {
-response.writeHead(200, {
-'Content-Type': 'text/html'
-});
+  request.on('end', () => {
+    switch (request.url) {
+      case '/':
+      fs.readFile('../htdocs/index.html', (err, data) => {
+        if (err) {
+          console.log(' 檔案讀取錯誤');
+        }
+        else {
+          response.writeHead(200, {
+            'Content-Type': 'text/html'
+          });
 
-response.write(data);
-response.end();
-}
-});
+          response.write(data);
+          response.end();
+        }
+      });
 
-break;
+      break;
 
-default:
-console.log(' 未定義的存取: ' + request.url);
+      case '/assets/css/styles.css':
+      fs.readFile('../htdocs/assets/css/styles.css', (err, data) => {
+        if (err) {
+          console.log(' 檔案讀取錯誤');
+        }
+        else {
+          response.writeHead(200, {
+            'Content-Type': 'text/css'
+          });
 
-response.end();
+          response.write(data);
+          response.end();
+        }
+      });
 
-break;
-}
-});
+      break;
+
+      case '/assets/png/SokobanClone_byVellidragon.png':
+      fs.readFile('../htdocs/assets/png/SokobanClone_byVellidragon.png', (err, data) => {
+        if (err) {
+          console.log(' 檔案讀取錯誤');
+        }
+        else {
+          response.writeHead(200, {
+            'Content-Type': 'image/png'
+          });
+
+          response.write(data);
+          response.end();
+        }
+      });
+
+      break;
+
+
+      default:
+      console.log(' 未定義的存取: ' + request.url);
+
+      response.end();
+
+      break;
+    }
+  });
 }).listen(8088);
 
 // log message to Console
